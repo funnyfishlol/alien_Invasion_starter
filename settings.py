4fe579e5210e044657509f8088c03da40b2d3f17
nate <strangetad666@gmail.com>
2025-11-16 20:06:13 -0500

    Refactor code for events and drawing

--- settings.py
+++ settings.py
@@ -6,6 +6,6 @@ class Settings:
         self.screen_h = 800
         self.FPS = 60
         self.bg_file = Path.cwd() / 'alien_Invasion_starter' / 'Assets' / 'images' / 'Starbasesnow.png'
-        self.ship_file = Path.cwd() / 'alien_Invasion_starter' / 'Assets' / 'images' / 'ship2.png'
-        self.ship_w = 40
-        self.ship_h = 60
+        self.ship_file = Path.cwd() / 'alien_Invasion_starter' / 'Assets' / 'images' / 'pngtree-x-wing-starfigher-star-wars-ija-collection-png-image_8656566.png'
+        self.ship_w = 60
+        self.ship_h = 90
