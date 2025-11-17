4a53300ca572078de072712fccba486e5ce918f0
nate <strangetad666@gmail.com>
2025-11-16 19:36:19 -0500

    created settings file and added background file to screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,19 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
-        pygame.display.set_caption("Alien Invasion")
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_w,self.settings.screen_h))
+        pygame.display.set_caption(self.settings.name)
+        
         self.running = True
+        self.clock = pygame.time.Clock()
+        self.bg = pygame.image.load(self.settings.bg_file)
+        self.bg = pygame.transform.scale(self.bg, (self.settings.screen_w, self.settings.screen_h))
 
     def run_game(self):
         while(self.running):
@@ -15,7 +22,10 @@ class AlienInvasion:
                     self.running = False
                     pygame.quit()
                     sys.exit()
-        pygame.display.flip()
+            
+            self.screen.blit(self.bg, (0,0))
+            pygame.display.flip()
+            self.clock.tick(self.settings.FPS)
 
 
 if __name__ == '__main__':
