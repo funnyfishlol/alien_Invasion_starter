cc1087a843ffb35fe02fca30eb9ff401849f65d2
nate <strangetad666@gmail.com>
2025-11-16 21:42:44 -0500

    Upload new images and sounds

--- settings.py
+++ settings.py
@@ -5,12 +5,12 @@ class Settings:
         self.screen_w = 1200
         self.screen_h = 800
         self.FPS = 60
-        self.bg_file = Path.cwd() / 'alien_Invasion_starter' / 'Assets' / 'images' / 'Starbasesnow.png'
+        self.bg_file = Path.cwd() / 'alien_Invasion_starter' / 'Assets' / 'images' / 'nebula.png'
         self.ship_file = Path.cwd() / 'alien_Invasion_starter' / 'Assets' / 'images' / 'pngtree-x-wing-starfigher-star-wars-ija-collection-png-image_8656566.png'
         self.ship_w = 60
         self.ship_h = 90
-        self.bullet_file = Path.cwd() / 'alien_Invasion_starter' / 'Assets' / 'images' / 'laserBlast.png'
-        self.laser_sound = Path.cwd() / 'alien_Invasion_starter' / 'Assets' / 'sound' / 'laser.mp3'
+        self.bullet_file = Path.cwd() / 'alien_Invasion_starter' / 'Assets' / 'images' / 'orangelaser.png'
+        self.laser_sound = Path.cwd() / 'alien_Invasion_starter' / 'Assets' / 'sound' / 'newlaser.mp3'
         self.bullet_speed = 7
         self.bullet_w = 25
         self.bullet_h = 80
